8a3cf5f7202309c48e5123de04c8fd33dde7fe88
Dongjin Kim <nyong9221@gmail.com>
2023-01-11 00:37:10 +0900

    🎉 Init : SpringBoot Initializing

---
+++ BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/UYouBooDanApplication.java
@@ -0,0 +1,13 @@
+package TeamBigDipper.UYouBooDan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UYouBooDanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UYouBooDanApplication.class, args);
+	}
+
+}
