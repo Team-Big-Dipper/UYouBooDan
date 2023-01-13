30c5c4bf279debc62d6a17586fb883fa5a27c4ff
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2023-01-13 22:20:13 +0900

    ✨ feat : ExceptionAdvice Setting (#2)
    Exception Advice 초기 세팅입니다.

---
+++ BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/global/exception/exceptionCode/ExceptionCode.java
@@ -0,0 +1,21 @@
+package TeamBigDipper.UYouBooDan.global.exception.exceptionCode;
+
+import lombok.Getter;
+
+public enum ExceptionCode {
+    /**
+     * 필요한 에러를 형식에 맞게 입력해주세요
+     */
+    NON_ACCESS_MODIFY(401, "수정권한이 없습니다.");
+
+    @Getter
+    private int status;
+    @Getter
+    private String message;
+
+    ExceptionCode(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+}
