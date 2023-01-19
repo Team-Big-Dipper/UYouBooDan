9e179d4335b19023f9b14360d61e5869eeb896c8
NR0617 <nara20210617@gmail.com>
2023-01-19 22:05:22 +0900

    :sparkles: feat: readVote 페이지 만드는 중

---
+++ FE/src/pages/readvote/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Sidebar from './sidebar';
+import ListPage from './listPage';
+import * as S from './style';
+
+
+
+const ReadVote = () => {
+  return (
+  <S.PageContainer>
+    <Sidebar />
+    <ListPage />
+  </S.PageContainer>
+    )
+}
+
+export default ReadVote;
---
+++ FE/src/pages/readvote/style.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const PageContainer = styled.div`
+    border: 1px solid red;
+    display: flex;
+`
+export const Sidebar = styled.div`
+  border: 1px solid red;
+  min-width: 300px;
+  display: flex;
+  flex-direction: column;
+`;
+export const VoteList = styled.div`
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`;
+
+export const CategoryHeader = styled.div`
+  border: 1px solid red;
+  display: flex;
+  justify-content: space-between;
+`
+
+export const CardItem = styled.div`
+    border: 1px solid blue;
+`
