70c6c8b3870385fa665f8ffaeda999f5e67e2d72
OH NARAE <94212747+NR0617@users.noreply.github.com>
2023-01-19 22:17:28 +0900

    [FE] 게시물 리스트 작성중 #11 (#14)
    확인했습니다

---
+++ FE/src/pages/readvote/sidebar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './style';
+
+const Sidebar = () => {
+  return (
+    <>
+      <S.Sidebar>사이드바</S.Sidebar>
+    </>
+  )
+}
+
+export default Sidebar;
---
+++ FE/src/pages/readvote/voteCards.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as S from './style';
+
+const VoteCards = () => {
+
+  return (
+    <S.CardItem>
+      epdlxj
+    </S.CardItem>
+  )
+}
+
+export default VoteCards;
