3ed3472612d1d715eda04599e4964b13036d36fe
NR0617 <nara20210617@gmail.com>
2023-01-21 21:47:46 +0900

    :sparkles: 게시물 리스트 웹버전 구현중

--- FE/src/pages/readvote/answerList.tsx
+++
@@ -1,5 +0,0 @@
-const AnswerList = () => {
-  return <div>댓글</div>;
-};
-
-export default AnswerList;
--- FE/src/pages/readvote/style.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const PageContainer = styled.div`
-  border: 1px solid red;
-  display: flex;
-  padding: 5% 20% 0 20%;
-  flex-direction: column;
-`;
