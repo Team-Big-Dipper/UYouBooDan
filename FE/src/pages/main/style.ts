db25fd22c2ba1ff80c4814f153422c54ef923a5d
devminoh <gmerit0116@naver.com>
2023-01-22 23:48:33 +0900

    :sparkles: feat: 메인페이지 레이아웃 진행중 #12

--- FE/src/pages/main/style.ts
+++ FE/src/pages/main/style.ts
@@ -1,5 +1,215 @@
 import styled from 'styled-components';
+import Frame from '../../../public/Mainimage.png'
+import HotFrame from '../../../public/HotFrame.png'
+import MainImageVote from '../../../public/MainImageVote.png'
+import Link from 'next/link';
 
 export const MainContainer = styled.div`
   flex: 1;
+`;
+
+export const MakeVoteContainer = styled.div`
+  width: 100%;
+  height: 40vh;
+  background-image: url(${Frame.src});
+  background-size: 100% 100%;
+  padding: 0 20%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const MakeVoteContent = styled.div`
+  width: fit-content;
+  div{
+    font-size: 1.5rem;
+    font-family: 'yg-jalnan' !important;
+    margin-bottom: 5px;
+    color: #242424;
+  }
+`;
+
+export const CreateVoteBtn = styled(Link)`
+  color: white;
+  background-color: #4285F4;
+  font-family: 'yg-jalnan' !important;
+  border: none;
+  height: 50px;
+  border-radius: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  cursor: pointer;
+  text-decoration: none;
+`;
+
+//진행중인투표
+export const ContinueVoteContainer = styled.div`
+  padding: 0 20%;
+  height: 40vh;
+  justify-content: center;
+  display: flex;
+  align-items: center;
+`;
+export const ContinueTitle = styled.div`  
+  width: 150px;
+ h2{
+  font-family: 'yg-jalnan' !important;
+  font-size: 1.2rem;
+  color: #242424;
+  line-height: 1.5rem;
+ }
+ div{
+  color: #667085;
+  margin-top: 10px;
+  font-size: 0.9rem;
+ }
+`;
+
+export const CarouselContainer = styled.div`
+  padding: 0 15px;
+  margin: 0 auto;
+  width: 75%;
+`;
+export const Crads = styled.ul`
+  display: grid;
+  grid-column-gap: 10px;
+  grid-auto-flow: column;
+  padding: 25px 0px;
+  list-style: none;
+  overflow-x: scroll;
+  scroll-snap-type: x mandatory;
+  &::-webkit-scrollbar {
+    height: 8px;
+    cursor: pointer;
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 92px;
+  }
+  &::-webkit-scrollbar-track{
+    border-radius: 92px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: #4285F4;
+  } 
+  &::-webkit-scrollbar-track {
+    background: #d6d9dc;
+  }
+`;
+
+export const Card = styled.li`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 130px;
+  width: 200px;
+  padding: 15px;
+  background: white;
+  border: 2px solid #d6d9dc;
+  border-radius: 12px;
+  /* box-shadow: 0 5px 15px rgba(0, 0, 0, 15%); */
+  scroll-snap-align: start;
+  transition: all 0.2s;
+  cursor: pointer;
+  &:hover {
+    border: 2px solid #4285F4;
+  }
+`;
+export const CardTitle = styled.h3`
+  font-size: 0.9rem;
+  color: #667085;
+  span{
+    color: white;
+    background-color: #4285F4;
+    padding: 2px 10px;
+    border-radius: 50px;
+    margin-left: 10px;
+  }
+`;
+export const CardContent = styled.div`
+  margin: 20px 0;
+  max-width: 85%;
+  font-weight: bold;
+`;
+export const AuthorDay = styled.div`
+  display: flex;
+  justify-content: space-between;
+  color: #667085;
+  font-size: 0.8rem;
+`;
+
+//핫한 투표
+export const HotContainer = styled.div`
+  height: 50vh;
+  background-image: url(${HotFrame.src});
+  background-size: 100% 100%;
+  padding: 0 20%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const HotTitle = styled.div`
+  color: #242424;
+  font-family: 'yg-jalnan' !important;
+  font-size: 1.2rem;
+  span{
+    color: #4285F4;
+    font-family: 'yg-jalnan' !important;
+  }
+`;
+
+//이미지투표+마감임박투표
+export const ImageDeadLineContainer = styled.div`
+  height: 90vh;
+  padding: 0 20%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ImageContainer = styled.div`
+  height: 80%;
+  width: 50%;
+  background-image: url(${MainImageVote.src});
+  background-size: 100% 100%;
+`;
+export const DeadLineContainer = styled.div`
+  width: 45%;
+`;
+export const DeadLineTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
+  .title{
+    color: #242424;
+    font-family: 'yg-jalnan' !important;
+    font-size: 1.2rem;
+  }
+  .more{
+    color: #667085;
+    font-size: 0.9rem;
+  }
+`;
+export const DeadLineTable = styled.table`
+  width: 100%;
+`;
+export const DeadLineCards = styled.tbody`
+  display: flex;
+  flex-direction: column;
+`;
+export const DeadLineCard = styled.tr`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 130px;
+  padding: 15px;
+  background: white;
+  border: 1px solid #d6d9dc;
+  scroll-snap-align: start;
+  transition: all 0.2s;
+  cursor: pointer;
+  &:hover {
+    border: 1px solid #4285F4;
+  }
 `;
