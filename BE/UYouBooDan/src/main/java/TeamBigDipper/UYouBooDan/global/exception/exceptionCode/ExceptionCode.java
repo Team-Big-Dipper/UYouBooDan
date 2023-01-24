2f381c06ec80cc01049d05ed3812df2fa50a25c9
Dongjin Kim <nyong9221@gmail.com>
2023-01-24 20:09:36 +0900

    ✨ feat : Member Service create, modify, 단일 find 서비스메소드 구현.    (예외처리를 위한 BusinessLogicException Case 추가)

--- BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/global/exception/exceptionCode/ExceptionCode.java
+++ BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/global/exception/exceptionCode/ExceptionCode.java
@@ -6,7 +6,8 @@ public enum ExceptionCode {
     /**
      * 필요한 에러를 형식에 맞게 입력해주세요
      */
-    NON_ACCESS_MODIFY(401, "수정권한이 없습니다.");
+    NON_ACCESS_MODIFY(401, "수정권한이 없습니다."),
+    MEMBER_NOT_FOUND(400, "존재하지 않는 계정입니다.");
 
     @Getter
     private int status;
