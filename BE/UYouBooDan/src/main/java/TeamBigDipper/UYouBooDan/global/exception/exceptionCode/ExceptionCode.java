87480c8dc8eb947378106abc70b3c41966165f7a
mingu0429 <mingu0429@gmail.com>
2023-01-24 22:33:29 +0900

    :sparkles: feat: ExceptionCode 추가

--- BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/global/exception/exceptionCode/ExceptionCode.java
+++ BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/global/exception/exceptionCode/ExceptionCode.java
@@ -6,7 +6,8 @@ public enum ExceptionCode {
     /**
      * 필요한 에러를 형식에 맞게 입력해주세요
      */
-    NON_ACCESS_MODIFY(401, "수정권한이 없습니다.");
+    NON_ACCESS_MODIFY(401, "수정권한이 없습니다."),
+    NOT_FOUND(404,"찾으신 내역이 일단 없습니다.");
 
     @Getter
     private int status;
