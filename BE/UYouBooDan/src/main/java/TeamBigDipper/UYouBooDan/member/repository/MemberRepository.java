f1c60f87fac65db8d2916397441833d78ac1c94e
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2023-01-24 22:30:24 +0900

    [BE] Member 기본 도메인 모델 구현 #17 (#27)

---
+++ BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/member/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package TeamBigDipper.UYouBooDan.member.repository;
+
+import TeamBigDipper.UYouBooDan.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
