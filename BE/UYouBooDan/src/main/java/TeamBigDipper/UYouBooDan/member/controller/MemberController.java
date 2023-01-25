0727ccee4ec41a712aefdd7fefdd313ed0a0bb0f
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2023-01-26 01:29:38 +0900

    [BE] Member 탈퇴, 삭제, 단일회원조회, 전체회원 페이지조회, 비밀번호 확인 API 구현 (#31)
    Member 도메인 대해 다음의 API가 추가되었습니다.
    
    Member 탈퇴 API : MEMBER_QUIT으로 상태변경
    Member 삭제 API : DB에서 계정 삭제
    Member 단일회원조회 API : 단일 계정에 대한 정보 조회
    Member 전체회원 페이지조회 API : 페이지네이션 적용 완료
    Member 비밀번호 확인 API 구현 API : password인코더 구현 후 추가작업 예정
    Member Email 중복확인 : 핸들러메서드만 작성 => 다음 작업 시 기능 구현예정
    Member Nickname 중복확인 : 핸들러메서드만 작성 => 다음 작업 시 기능 구현예정
    
    유의사항 
    현재 시큐리티 미구현으로 PathVariable에 member_id가 필요합니다.
    추후 시큐리티 적용 후 JWT를 이용한 자체 검증으로 변경 예정입니다.
    이전 PR에서 있었던 상태관리 및 BaseTime 이슈를 해결했습니다.
    BaseTime사용을 위해 엔트리 포인트에 이벤트 리스너 애너테이션을 추가했습니다.

--- BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/member/controller/MemberController.java
+++ BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/member/controller/MemberController.java
@@ -4,18 +4,19 @@ import TeamBigDipper.UYouBooDan.global.dto.MultiResDto;
 import TeamBigDipper.UYouBooDan.global.dto.SingleResDto;
 import TeamBigDipper.UYouBooDan.member.dto.MemberPatchReqDto;
 import TeamBigDipper.UYouBooDan.member.dto.MemberPostReqDto;
+import TeamBigDipper.UYouBooDan.member.dto.MemberResDto;
+import TeamBigDipper.UYouBooDan.member.dto.PasswordReqDto;
 import TeamBigDipper.UYouBooDan.member.entity.Member;
 import TeamBigDipper.UYouBooDan.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
@@ -46,9 +47,9 @@ public class MemberController {
      * @param memberId : 시큐리티 구성 전 임시 Variable
      * @return void
      */
-    @PatchMapping("/edit/{member_id}")
+    @PatchMapping("/edit/{member-id}")
     public ResponseEntity<SingleResDto<String>> patchMember (@RequestBody MemberPatchReqDto memberPatchReqDto,
-                                                             @PathVariable("member_id") Long memberId) {
+                                                             @PathVariable("member-id") Long memberId) {
         memberService.modifyMember(memberPatchReqDto.toEntity(), memberId);
         return new ResponseEntity<>(new SingleResDto<>("Success Patch"), HttpStatus.OK);
     }
@@ -58,25 +59,39 @@ public class MemberController {
      * Delete 요청
      * 애너테이션은 delete 요청을 받을 것이므로, DeleteMapping으로 받음
      * 상태변환을 할 예정이므로, HttpStatus는 NO_CONTENT가 아닌 OK로 함
-     * @return "data" : "String"
+     * 회원탈퇴(withdraw) 후 성공 메세지 반환
+     * @return "data" : "성공 메세지"
      */
-    @DeleteMapping("/remove")
-    public ResponseEntity<SingleResDto<String>> deleteMember () {
-        /**
-         * 로그인한 사용자 Id값을 받아 상태 변환 예정
-         */
+    @DeleteMapping("/remove/{member-id}")
+    public ResponseEntity<SingleResDto<String>> withdrawMember (@PathVariable("member-id") Long memberId) {
+        memberService.withdrawMember(memberId);
+
+        return new ResponseEntity<>(new SingleResDto<>("Success Withdraw"), HttpStatus.OK);
+    }
+
+
+    /**
+     * 회원 삭제 메소드
+     * @return 삭제 성공 메세지
+     */
+    @DeleteMapping("/delete/{member-id}")
+    public ResponseEntity<SingleResDto<String>> deleteMember (@PathVariable("member-id") Long memberId) {
+        memberService.removeMember(memberId);
+
         return new ResponseEntity<>(new SingleResDto<>("Success Delete"), HttpStatus.OK);
     }
 
 
     /**
      * 단일 Get 요청
-     * @return "data" : "String"
+     * @return "data" : "단일 객체에 대한 응답정보"
      */
-    @GetMapping("/find")
-    public ResponseEntity<SingleResDto<String>> getMember () {
+    @GetMapping("/find/{member-id}")
+    public ResponseEntity<SingleResDto<MemberResDto>> getMember (@PathVariable("member-id") Long memberId) {
+        Member member = memberService.findMember(memberId);
+        MemberResDto response = new MemberResDto(member);
 
-        return new ResponseEntity<>(new SingleResDto<>("Success Get"), HttpStatus.OK);
+        return new ResponseEntity<>(new SingleResDto<>(response), HttpStatus.OK);
     }
 
 
@@ -85,13 +100,54 @@ public class MemberController {
      * @return "data" : "String"
      */
     @GetMapping("/find-all")
-    public ResponseEntity<MultiResDto<String>> getMembers (Pageable pageable) {
-        List<String> list = new ArrayList<>();
-        int count = 0;
-        while(count++<30) list.add("Member " + count);
-        Page<String> page = new PageImpl<>(list, pageable, list.size());
+    public ResponseEntity<MultiResDto<MemberResDto>> getMembers (Pageable pageable) {
+        Page<Member> page = memberService.findMembers(pageable);
+        Page<MemberResDto> response = page.map(MemberResDto::new);
+        List<MemberResDto> list = response.stream().collect(Collectors.toList());
 
         return new ResponseEntity<>(new MultiResDto<>(list, page), HttpStatus.OK);
+    }
+
+
+    /**
+     * password 확인 : 성공시 String 메세지, 실패시 BusinessLoginException 발생
+     * 추후 Password 인코더 구현 시 추가 작업 예정
+     */
+    @PostMapping("/verify/{member-id}")
+    public ResponseEntity<SingleResDto<String>> checkPassword(@PathVariable("member-id") Long memberId,
+                                                              @RequestBody PasswordReqDto passwordDto) {
+        memberService.verifyPassword(passwordDto.getPassword(), memberId);
+
+        return new ResponseEntity<>(new SingleResDto<>("Verify Success."),HttpStatus.OK);
+    }
+
+
+/**
+ * email확인 및 nickname 확인 API 다음작업시 구현예정 (현재는 Handler메소드만 구현)
+ */
+
+    /**
+     * email 중복확인
+     * @param email
+     * @return
+     */
+    @GetMapping("/verify-email")
+    public ResponseEntity<SingleResDto<String>> checkEmail(@RequestParam(required = false) String email) {
+        memberService.verifyEmail(email);
+
+        return new ResponseEntity<>(new SingleResDto<>("Verify Success."),HttpStatus.OK);
+    }
+
+    /**
+     * 닉네임 중복확인
+     * @param nickname
+     * @return
+     */
+    @GetMapping("/verify-nickname")
+    public ResponseEntity<SingleResDto<String>> checkNickname(@RequestParam(required = false) String nickname) {
+        memberService.verifyNickname(nickname);
+
+        return new ResponseEntity<>(new SingleResDto<>("Verify Success."),HttpStatus.OK);
     }
 
 }
--- BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/member/service/MemberService.java
+++ BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/member/service/MemberService.java
@@ -5,6 +5,8 @@ import TeamBigDipper.UYouBooDan.global.exception.exceptionCode.ExceptionCode;
 import TeamBigDipper.UYouBooDan.member.entity.Member;
 import TeamBigDipper.UYouBooDan.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,24 +35,43 @@ public class MemberService {
         return memberRepository.save(member);
     }
 
-
+    /**
+     * 들어오는 값에 따라, 다음의 Patch 요청을 개별적으로 관리 가능합니다. (null이 아닐경우 수정되는 방식 미적용)
+     * 1. 회원 정보 변경 가능
+     * 2. 회원 상태 변경 가능
+     * 비즈니스 로직은 Member Entity 내에서 처리
+     * @param memberId 추후 JWT에서 추출 예정
+     */
     @Transactional
     public void modifyMember(Member member, Long memberId) {
-        Member existMember = (memberRepository.findById(memberId)).get();
+        Member existMember = new Member().verifyMember(memberRepository.findById(memberId));
         Optional.ofNullable(member.getPassword()).ifPresent(existMember::modifyPassword);
         Optional.ofNullable(member.getNickname()).ifPresent(existMember::modifyNickname);
         Optional.ofNullable(member.getProfile()).ifPresent(existMember::modifyProfile);  // 현재 profile의 경우 단순 URI상태. 추후 파일로 변경 예정
+        Optional.ofNullable(member.getMemberStatus()).ifPresent(existMember::modifyMemberStatus);
 
         memberRepository.save(existMember);
     }
 
 
     /**
-     * 추후 구현할 기본 메서드
-     * 1. removeMember => 상태 ENUM 문제 해결 후 구현 예정
-     * 2. findMembers => DB 구성 완료 후 구현 예정 (현재는 Controller 내 더미데이터 반환)
+     * Member 완전 삭제시 사용
      */
+    @Transactional
+    public void removeMember(Long memberId) {
+        Member verifyMember = new Member().verifyMember(memberRepository.findById(memberId));
+        memberRepository.delete(verifyMember);
+    }
 
+    /**
+     * Member Entity클래스 내 구현한 회원탈퇴 메소드(withdrawMember 호출)
+     */
+    @Transactional
+    public void withdrawMember (Long memberId) {
+        Member verifyMember = new Member().verifyMember(memberRepository.findById(memberId));
+        verifyMember.withdrawMember();
+        memberRepository.save(verifyMember);
+    }
 
     /**
      * 현재는 Controller에서 String 반환중
@@ -58,9 +79,39 @@ public class MemberService {
      * @return : 1명의 member 정보를 반환 => 맞춰서 ResponseDto 제작 예정
      */
     public Member findMember(Long memberId) {
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(()->new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+        Member member = new Member().verifyMember(memberRepository.findById(memberId));
         return member;
     }
 
+
+    /**
+     * 전체 회원 조회용 (Default는 10계정)
+     * @param pageable : page, size, sort 등 사용 가능
+     * @return Page 구조
+     */
+    public Page<Member> findMembers (Pageable pageable) {
+        return memberRepository.findAll(pageable);
+    }
+
+    /**
+     * password 일치 여부 조회 메소드
+     * @param memberId
+     */
+    public void verifyPassword (String password, Long memberId) {
+        Member member = new Member().verifyMember(memberRepository.findById(memberId));
+        member.checkPassword(password);
+    }
+
+
+    public void verifyEmail (String email) {
+        /**
+         * 중복확인 쿼리 메서드 구현하기
+          */
+    }
+
+    public void verifyNickname (String nickname) {
+        /**
+         * 중복확인 쿼리 메서드 구현하기
+         */
+    }
 }
