2b1d6324ff6054a92a6544a0e08ee84d9fe41d83
devminoh <gmerit0116@naver.com>
2023-01-25 23:02:05 +0900

    :sparkles: feat: mockVote 생성

---
+++ FE/src/mocks/data/vote.ts
@@ -0,0 +1,11 @@
+const mockVote = [
+  {
+    category: 'a',
+    title: '제목입니다',
+    content: '설명입니다',
+    topicVoteItems: ['1','2','3'],
+    closedAt: '2023-02-13 02:12:34'
+  },
+];
+
+export default mockVote;
