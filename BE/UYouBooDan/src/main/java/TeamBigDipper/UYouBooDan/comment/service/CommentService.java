6b834cfb21153444cd0056031e26415e1690dece
mingu0429 <mingu0429@gmail.com>
2023-01-25 23:23:10 +0900

    :sparkles: feat: likeComment 댓글 좋아요 기능 구현

--- BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/comment/service/CommentService.java
+++ BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/comment/service/CommentService.java
@@ -1,6 +1,8 @@
 package TeamBigDipper.UYouBooDan.comment.service;
 
 import TeamBigDipper.UYouBooDan.comment.entity.Comment;
+import TeamBigDipper.UYouBooDan.comment.entity.CommentLike;
+import TeamBigDipper.UYouBooDan.comment.repository.CommentLikeRepository;
 import TeamBigDipper.UYouBooDan.comment.repository.CommentRepository;
 import TeamBigDipper.UYouBooDan.global.exception.dto.BusinessLogicException;
 import TeamBigDipper.UYouBooDan.global.exception.exceptionCode.ExceptionCode;
@@ -17,6 +19,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
+    private final CommentLikeRepository commentLikeRepository;
 
     /**
      * 댓글 생성
@@ -80,5 +83,37 @@ public class CommentService {
         Page<Comment> page = commentRepository.findAllByTopicIdOrderByCreatedAtDesc(pageable, topicId);
 
         return page;
+    }
+
+    /**
+     * 댓글 좋아요
+     * @param commentId
+     * @param memberId
+     * @return
+     */
+    public CommentLike likeComment(Long commentId, Long memberId){// commentLike 엔티티 내 메서드로 옮겨서 만들어서 쓰자 & 레포지토리 접근은 냅두고
+        Comment savedComment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.NOT_FOUND));
+
+        if(commentLikeRepository.existsByCommentIdAndMemberId(commentId, memberId)){// commentLike 존재할 경우
+            CommentLike savedCommentLike = commentLikeRepository.findByCommentIdAndMemberId(commentId, memberId);
+            if(savedCommentLike.getCommentLikeStatus()){//좋아요로 되어있다면
+                savedCommentLike.setCommentLikeStatus(false);// 좋아요 취소
+                savedCommentLike.setModifiedAt(LocalDateTime.now());
+            }else{
+                savedCommentLike.setCommentLikeStatus(true);// 좋아요가 안되어있으면 좋아요
+                savedCommentLike.setModifiedAt(LocalDateTime.now());
+            }
+            return commentLikeRepository.save(savedCommentLike);
+
+        }else{// commentLike 없을 경우: 새로 생성
+            CommentLike commentLike = new CommentLike();
+            commentLike.setCommentId(commentId);
+            commentLike.setMemberId(memberId);
+            commentLike.setCommentLikeStatus(true);
+            commentLike.setCreatedAt(LocalDateTime.now());
+            commentLike.setModifiedAt(LocalDateTime.now());
+            return commentLikeRepository.save(commentLike);
+        }
     }
 }
