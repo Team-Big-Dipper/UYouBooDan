8a89cda10365487f311954f6a9d194b03f327fba
Dongjin Kim <nyong9221@gmail.com>
2023-01-27 17:53:13 +0900

    :sparkles: feat: Spring Security JwtAuthenticationFilter, JwtVerificationFilter 클래스 구현

---
+++ BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/global/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,73 @@
+package TeamBigDipper.UYouBooDan.global.security.filter;
+
+import TeamBigDipper.UYouBooDan.global.security.dto.LoginDto;
+import TeamBigDipper.UYouBooDan.global.security.jwt.JwtTokenizer;
+import TeamBigDipper.UYouBooDan.member.entity.Member;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenizer jwtTokenizer;
+
+    @Override
+    @SneakyThrows  // throws를 이용한 예외 처리를 대신해주는 애너테이션
+    public Authentication attemptAuthentication (HttpServletRequest request, HttpServletResponse response) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        LoginDto loginDto;
+
+        try {
+            loginDto = objectMapper.readValue(request.getInputStream(), LoginDto.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword());
+
+        return authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+
+    }
+
+
+    /**
+     * 인증된 유저에게 HttpServlerResponse를 통해 AccessToken과 RefreshToken 전달해주기 위해 토큰 생성 후 담아주는 부분
+     * @param authResult the object returned from the <tt>attemptAuthentication</tt>
+     */
+    @Override
+    @SneakyThrows
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                            FilterChain filterChain, Authentication authResult) {
+
+        Member authenticatedMember = (Member) authResult.getPrincipal();
+        String accessToken = jwtTokenizer.delegateAccessToken(authenticatedMember);
+        String refreshToken = jwtTokenizer.delegateRefreshToken(authenticatedMember);
+        response.setHeader("Authentication", "Bearer " + accessToken);
+        response.setHeader("RefreshToken", refreshToken);
+        this.getSuccessHandler().onAuthenticationSuccess(request, response, authResult);
+    }
+
+
+    @SneakyThrows
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                              AuthenticationException failed) {
+
+        super.unsuccessfulAuthentication(request, response, failed);
+    }
+
+}
