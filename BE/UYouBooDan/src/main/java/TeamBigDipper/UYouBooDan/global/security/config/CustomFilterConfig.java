f1121a97ca6f1f2791dc846824d67e250f6640fe
Dongjin Kim <nyong9221@gmail.com>
2023-01-27 18:13:46 +0900

    :recycle: refactor: Spring Security CustomFilterConfig 클래스 수정 (Filter 적용)

--- BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/global/security/config/CustomFilterConfig.java
+++ BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/global/security/config/CustomFilterConfig.java
@@ -1,8 +1,13 @@
 package TeamBigDipper.UYouBooDan.global.security.config;
 
+import TeamBigDipper.UYouBooDan.global.security.filter.JwtAuthenticationFilter;
+import TeamBigDipper.UYouBooDan.global.security.filter.JwtVerificationFilter;
+import TeamBigDipper.UYouBooDan.global.security.handler.CustomAuthenticationFailureHandler;
+import TeamBigDipper.UYouBooDan.global.security.handler.CustomAuthenticationSuccessHandler;
 import TeamBigDipper.UYouBooDan.global.security.jwt.JwtTokenizer;
 import TeamBigDipper.UYouBooDan.global.security.util.CustomAuthorityUtils;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,16 +19,34 @@ public class CustomFilterConfig extends AbstractHttpConfigurer<CustomFilterConfi
     private final CustomAuthorityUtils customAuthorityUtils;
 //    private final RedisTemplate redisTemplate; // 추후 레디스 적용 시 활성화
 
+
     @Override
-    public void configure(HttpSecurity builder) throws Exception {
+    @SneakyThrows
+    public void configure(HttpSecurity builder) {
+
         AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
 
         /**
          * 인증 처리 필터
          */
-//        JwtAutheticationFilter jwtAutheticationFilter = new JwtAutheticationFilter(authenticationManager, jwtTokenizer);
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenizer);
 
+        /**
+         * 로그인을 위한 URL Path 설정하는 위치
+         */
+        jwtAuthenticationFilter.setFilterProcessesUrl("/auth/login");
+
+        /**
+         * 구현한 AuthenticationFilter 추가하는 위치
+         */
+        jwtAuthenticationFilter.setAuthenticationSuccessHandler(new CustomAuthenticationSuccessHandler());
+        jwtAuthenticationFilter.setAuthenticationFailureHandler(new CustomAuthenticationFailureHandler());
+        JwtVerificationFilter jwtVerificationFilter = new JwtVerificationFilter(jwtTokenizer, customAuthorityUtils); // ,redisTemplate); // 레디스 적용시 활성화 예정
+
+        /**
+         * 생성한 필터 객체 세팅 후, builder를 이용해 필터 병합
+         */
+        builder.addFilter(jwtAuthenticationFilter).addFilterAfter(jwtVerificationFilter, JwtAuthenticationFilter.class);
 
     }
-
 }
