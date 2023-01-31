1f4764840c67a462e2796a1c703e7430ab9937e7
ZeroMin-K <ymkim9510@gmail.com>
2023-01-31 18:17:46 +0900

    :sparkles: feat: 투표 게시글 작성 기능 - TopicPostReqDto 수정
    - 엔티티 클래스 연관관계 매핑으로 인한 TopicPostReqDto 수정

--- BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/topic/dto/TopicPostReqDto.java
+++ BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/topic/dto/TopicPostReqDto.java
@@ -1,10 +1,13 @@
 package TeamBigDipper.UYouBooDan.topic.dto;
 
+import TeamBigDipper.UYouBooDan.member.entity.Member;
 import TeamBigDipper.UYouBooDan.topic.entity.Topic;
 import TeamBigDipper.UYouBooDan.topic.entity.TopicVoteItem;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,50 +16,61 @@ public class TopicPostReqDto {
     private String category;                // 카테고리
     private String title;                   // 제목
     private String content;                 // 내용
-    private LocalDateTime closedAt;         // 마감일
+    private String closedAt;         // 마감일
 
     private List<TopicVoteItemPostDto> topicVoteItems;    // 투표 항목
 
     // 투표 항목에 대한 DTO 클래스
     @Getter
     public static class TopicVoteItemPostDto {
-        private Long topicVoteItemId;           // 투표 항목 ID
         private String topicVoteItemName;       // 투표 항목 이름
     }
 
     /**
      * 투표 게시글 Topic Entity 클래스로 변환하는 메서드
-     * @param memberId 작성자 id
+     * @param member 작성자 Member 클래스
      * @return Topic Entity 클래스
      */
-    public Topic toTopic(Long memberId) {
+    public Topic toTopic(Member member) {
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
         Topic topic = Topic.builder()
                 .title(this.title)
                 .content(this.content)
                 .category(this.category)
-                .closedAt(this.closedAt)
-                .memberId(memberId)
+                .closedAt(LocalDateTime.parse(this.closedAt, timeFormatter))
+                .member(member)
                 .build();
 
         // 투표 게시글 생성 상태로 변환
         topic.changeTopicStatusActive();
 
+        // 투표 게시글에 투표 항목 추가
+        topicVoteItems.stream()
+                .map(topicVoteItemPostDto -> {
+                    TopicVoteItem topicVoteItem = TopicVoteItem.builder()
+                                    .topic(topic)
+                                    .topicVoteItemName(topicVoteItemPostDto.getTopicVoteItemName())
+                                    .build();
+                    return topicVoteItem;})
+                .forEach(topicVoteItem -> topic.addTopicVoteItem(topicVoteItem));
+
         return topic;
     }
 
-    /**
-     * 투표 항목 TopicVoteItem Entity 클래스를 원소로 갖는 리스트로 변환하는 메서드
-     * @param topicId 투표 게시글 ID
-     * @return TopicVoteItem Entity 클래스를 원소로 갖는 리스트
-     */
-    public List<TopicVoteItem> toTopicVoteItems(Long topicId) {
-        return topicVoteItems.stream()
-                .map(topicVoteItemPostDto -> {
-                    return TopicVoteItem.builder()
-                            .topicId(topicId)
-                            .topicVoteItemName(topicVoteItemPostDto.getTopicVoteItemName())
-                            .build();
-                }).collect(Collectors.toList());
-    }
+//    /**
+//     * 투표 항목 TopicVoteItem Entity 클래스를 원소로 갖는 리스트로 변환하는 메서드
+//     * @param topicId 투표 게시글 ID
+//     * @return TopicVoteItem Entity 클래스를 원소로 갖는 리스트
+//     */
+//    public List<TopicVoteItem> toTopicVoteItems(Long topicId) {
+//        return topicVoteItems.stream()
+//                .map(topicVoteItemPostDto -> {
+//                    return TopicVoteItem.builder()
+//                            .topicId(topicId)
+//                            .topicVoteItemName(topicVoteItemPostDto.getTopicVoteItemName())
+//                            .build();
+//                }).collect(Collectors.toList());
+//    }
 
 }
