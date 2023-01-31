4180788f141f06eb60bde7e151a4cdd2dc5350dc
ZeroMin-K <ymkim9510@gmail.com>
2023-01-31 17:51:32 +0900

    :sparkles: feat: 투표 게시글 작성 기능 - TopicVoteItemRepository 인터페이스 추가
    - 투표 항목 저장하는 TopicVoteItemRepository 인터페이스 추가

---
+++ BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/topic/repository/TopicVoteItemRepository.java
@@ -0,0 +1,7 @@
+package TeamBigDipper.UYouBooDan.topic.repository;
+
+import TeamBigDipper.UYouBooDan.topic.entity.TopicVoteItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TopicVoteItemRepository extends JpaRepository<TopicVoteItem, Long> {
+}
