cc10578d65aea12c11ebb36c1bb903979b91e02f
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2023-02-01 00:52:36 +0900

    [BE] 투표 게시글 작성하기 기능 구현 #29  (#58)

--- BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/topic/entity/Topic.java
+++ BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/topic/entity/Topic.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,38 +31,54 @@ public class Topic extends BaseTimeEntity {
 
     // private Long topicVoteId;            // 투표 Id
 
-    @Enumerated(EnumType.STRING)
-     private TopicStatus topicStatus;     // 투표 게시글 상태
+    @Enumerated(EnumType.STRING)         // Enum을 String으로 저장
+    private TopicStatus topicStatus;     // 투표 게시글 상태
 
+    @Enumerated(EnumType.STRING)            // Enum을 String으로 저장
     private Category category;              // 카테고리
 
-    private LocalDateTime closedAt;         // 마감 날짜
+    private LocalDateTime closedAt;         // 마감 날짜 (yyyy-MM-dd HH:mm:ss ex. 2023-01-01 13:00:00)
 
-    @OneToMany(mappedBy = "topic", cascade = CascadeType.PERSIST)
+    @OneToMany(mappedBy = "topic", cascade = CascadeType.PERSIST)       // 일대다 맵핑, PERSIST로 TopicVoteItem도 같이 저장
     private List<TopicVoteItem> topicVoteItems;      // 투표 항목
 
     @Builder
     public Topic(String title, String content, String category,
-                 Member member, LocalDateTime closedAt) {
+                 Member member, String closedAt) {
         this.title = title;
         this.content = content;
-        this.closedAt = closedAt;
-        this.category = Category.valueOf(category);
+        this.closedAt = LocalDateTime.parse(closedAt, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        this.category = Category.nameOf(category);
         this.member = member;
         topicVoteItems = new ArrayList<>();
     }
 
+    /**
+     * 투표 항목 추가
+     * @param topicVoteItem 투표항목 클래스
+     */
     public void addTopicVoteItem(TopicVoteItem topicVoteItem) {
         topicVoteItems.add(topicVoteItem);
     }
 
+    /**
+     * 투표 게시글을 활성화 상태로 전환
+     */
     public void changeTopicStatusActive() {
         topicStatus = TopicStatus.ACTIVE;
     }
+
+    /**
+     * 투표 게시글을 삭제 상태로 변환
+     */
     public void changeTopicStatusRemoved() {
         topicStatus = TopicStatus.REMOVED;
     }
 
+    /**
+     * 카테고리 enum 클래스
+     */
+    @Getter
     public enum Category {
         Food("음식")
         ;
@@ -72,21 +89,34 @@ public class Topic extends BaseTimeEntity {
         Category(String categoryName) {
             this.categoryName = categoryName;
         }
+
+        /**
+         * 한글로된 카테고리 이름을 Category Enum 으로 변경
+         * @param categoryName 한글로된 카테고리 이름
+         * @return Category Enum
+         */
+        public static Category nameOf(String categoryName) {
+            for (Category category : Category.values()) {
+                if (category.getCategoryName().equals(categoryName)){
+                    return category;
+                }
+            }
+            return null;
+        }
     }
 
+    /**
+     * 게시글 상태 관리
+     */
     public enum TopicStatus {
-        ACTIVE(0, "활성화"),
-        REMOVED(1, "삭제된 게시글")
+        ACTIVE("활성화"),
+        REMOVED("삭제된 게시글")
         ;
-
-        @Getter
-        private int code;
 
         @Getter
         private String message;
 
-        TopicStatus(int code, String message) {
-            this.code = code;
+        TopicStatus(String message) {
             this.message = message;
         }
     }
