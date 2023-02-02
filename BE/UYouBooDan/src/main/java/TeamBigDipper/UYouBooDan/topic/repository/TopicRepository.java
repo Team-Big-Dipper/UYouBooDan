00481718bc30a622d4d578a998d75b420c200772
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2023-02-02 23:05:33 +0900

    [BE] 진행 중인/마감 임박/종료된 투표 게시글 목록 조회 기능 구현 #63 (#69)

--- BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/topic/repository/TopicRepository.java
+++ BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/topic/repository/TopicRepository.java
@@ -5,7 +5,40 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface TopicRepository extends JpaRepository<Topic, Long> {
-    // 투표 게시글 전체 목록을 Pagination 이용해서 작성 날짜 역순으로 조회
+    /**
+     * 투표 게시글 전체 목록을 Pagination 이용해서 작성 날짜 역순으로 조회
+     * @param pageable Pageable 객체
+     * @return Pagination 적용된 Page Topic 객체
+     */
     Page<Topic> findAllByOrderByCreatedAtDesc(Pageable pageable);
+
+    /**
+     * 진행 중인 투표 게시글 목록을 Pagination 적용해서 작성일 내림차순으로 조회
+     * @param now 현재 날짜 : LocalDateTime 객체
+     * @param pageable pagination 객체
+     * @return Pagination 적용된 Topic 객체
+     */
+    Page<Topic> findAllByClosedAtIsAfterOrderByCreatedAtDesc(LocalDateTime now, Pageable pageable);
+
+    /**
+     * 마감 임박 투표 게시글 목록을 Pagination 적용해서 현재날짜와 마감 임박까지 마감일 오름차순으로 조회
+     * @param start 현재 날짜 : LocalDateTime 객체
+     * @param end 현재 날짜로부터 마감임박 제한 날짜 : LocalDateTime 객체
+     * @param pageable Pagination 객체
+     * @return Pagination 적용된 Topic 객체
+     */
+    Page<Topic> findAllByClosedAtBetweenOrderByClosedAtAsc(LocalDateTime start, LocalDateTime end, Pageable pageable);
+
+    // TODO: 전체 게시글 중 핫토픽 조회
+
+    /**
+     * 투표 마감된 투표 게시글 목록을 Pagination 적용해서 작성일 내림차순으로 조회
+     * @param now 현재 날짜 : LocalDateTime 객체
+     * @param pageable Pagination 객체
+     * @return Pagination 적용된 Topic 객체
+     */
+    Page<Topic> findAllByClosedAtIsBeforeOrderByCreatedAtDesc(LocalDateTime now, Pageable pageable);
 }
