6807e90f064821f9dbebd317fd41b22e9ccbabea
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2023-02-07 22:51:30 +0900

    [BE] GoogleLogin, Guest Login, MemberInfo Topic API,  MemberInfo Comment API 구현 (#89)
    API 요청은 Postman API 문서에 최신화 해두었습니다.
    
    MemberInfo Topic API : bestVote를 위한 static method는 추후 Vote 로직 완성 후 활성화 될 예정입니다. (현재는 null값으로 반환)
    
    CommentRepository에 쿼리메소드 추가에 의한 Conflict를 수정했습니다. 추후 Comment부분 conflict 발생 시, 아래 코드 부분을 참고하시기 바랍니다.
    Page findAllByMemberIdOrderByCreatedAtDesc(Pageable pageable, Long memberId);
    
    /**
    
    *** Comment 엔티티에 member Id에 대한 매핑 필요 ***
    내가 쓴 댓글 목록을 Pagination 적용하여 작성일 역순으로 조회
    @param memberId 현재 로그인 한 유저의 식별자
    @param pageable Pagination 파라미터
    @return
    */
    @query(value = "SELECT * FROM Comment comment WHERE comment.memberId =:memberId ORDER BY createdAt DESC", nativeQuery = true)
    Page findAllByMemberIdOrderByCreatedAtDesc(@param("memberId") Long memberId, Pageable pageable);
    }
    image
    
    아래 yml 코드를 추가해야 정상 동작합니다.
    oauth:
    ...
    google:
    clientId: 950144848706-cn38ajrvu1ef8ivojk8ald9534rnjuq4.apps.googleusercontent.com
    clientSecret: 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
    scope: profile,email,openid
    redirectUrl: http://localhost:3000/auth/googleredirect

---
+++ BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/global/oauth2/google/GoogleLoginDto.java
@@ -0,0 +1,29 @@
+package TeamBigDipper.UYouBooDan.global.oauth2.google;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class GoogleLoginDto {
+
+    private String iss;
+    private String azp;
+    private String aud;
+    private String sub;
+    private String email;
+    private String emailVerified;
+    private String atHash;
+    private String name;
+    private String picture;
+    private String givenName;
+    private String familyName;
+    private String locale;
+    private String iat;
+    private String exp;
+    private String alg;
+    private String kid;
+    private String typ;
+
+
+}
