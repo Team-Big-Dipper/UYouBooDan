65c1f31da9476eba4730cee91e202d7662ffed2d
ZeroMin-K <ymkim9510@gmail.com>
2023-02-09 22:12:53 +0900

    :sparkles: feat: 투표하기 기능 투표 TopicVote 엔티티 클래스 수정, 보완 #70

--- BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/topic/entity/TopicVote.java
+++ BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/topic/entity/TopicVote.java
@@ -1,22 +1,52 @@
 package TeamBigDipper.UYouBooDan.topic.entity;
 
 import TeamBigDipper.UYouBooDan.global.auditing.BaseTimeEntity;
+import TeamBigDipper.UYouBooDan.member.entity.Member;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class TopicVote extends BaseTimeEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long topicVoteId;       // 투표 ID
 
-    // 외래 키
-    // 연관 관계
-    private Long topicVoteItemId;    // 투표 항목 ID
+    @ManyToOne                                  // 투표 항목과 다대일 관계
+    @JoinColumn(name = "topic_vote_item_id")    // 외래키 - 투표 항목ID
+    private TopicVoteItem topicVoteItem;        // 투표 항목
 
-    // 외래 키
-    // 연관 관계
-    private Long memberId;           // 투표자 ID
+    @ManyToOne                                  // 사용자와 다대일 관계
+    @JoinColumn(name = "member_id")             // 외래키 - 사용자 ID
+    private Member member;                      // 투표자
+
+    @ManyToOne                                  // 투표 게시글과 다대일 관계
+    @JoinColumn(name = "topic_id")              // 외래키 - 투표 게시글ID
+    private Topic topic;                        // 투표 게시글
+
+    /**
+     * TopicVote 클래스 생성자
+     * @param topic 투표 게시글 객체 Topic
+     * @param member 사용자 객체 Member
+     * @param topicVoteItem 투표 항목 객체 TopicVoteItem
+     */
+    @Builder
+    public TopicVote(Topic topic, Member member,
+                     TopicVoteItem topicVoteItem) {
+        this.topic = topic;
+        this.topicVoteItem = topicVoteItem;
+        this.member = member;
+    }
+
+    /**
+     * 사용자가 투표 항목에 투표
+     * @param topicVoteItem 투표할 투표 항목
+     */
+    public void voteTopicVoteItem(TopicVoteItem topicVoteItem) {
+        this.topicVoteItem = topicVoteItem;
+    }
 }
