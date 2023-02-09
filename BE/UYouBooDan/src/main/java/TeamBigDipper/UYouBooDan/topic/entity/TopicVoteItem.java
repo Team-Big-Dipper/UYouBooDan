d794eb8b3c603e4a715cbd3a9e0263d0c656fc6e
ZeroMin-K <ymkim9510@gmail.com>
2023-02-09 22:13:10 +0900

    :sparkles: feat: 투표하기 기능 투표 항목 TopicVoteItem 엔티티 클래스 수정, 보완 #70

--- BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/topic/entity/TopicVoteItem.java
+++ BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/topic/entity/TopicVoteItem.java
@@ -1,11 +1,13 @@
 package TeamBigDipper.UYouBooDan.topic.entity;
 
 import TeamBigDipper.UYouBooDan.global.auditing.BaseTimeEntity;
+import TeamBigDipper.UYouBooDan.member.entity.Member;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -23,18 +25,43 @@ public class TopicVoteItem extends BaseTimeEntity {
 
     private String topicVoteItemName;   // 투표 항목 이름
 
-//    @OneToMany(mappedBy = "topic")
-//    private List<TopicVote> topicVotes;     // 투표항목에 누가 투표했는지
+    @OneToMany(mappedBy = "topicVoteItem", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private List<TopicVote> topicVotes;         // 투표항목에 누가 투표했는지
+
+//    @Transient
+//    private Integer numberOfVotes;             // 투표 수
 
     @Transient
-    private Integer numberOfVotes;             // 투표 수
-
-    @Transient
-    private Boolean isTopicVoteItemVoted;       // 조회하는 사람이 투표했는지 여부
+    private Boolean topicVoteItemVoted;       // 조회하는 사람이 투표했는지 여부
 
     @Builder
     public TopicVoteItem(Topic topic, String topicVoteItemName) {
         this.topic = topic;
         this.topicVoteItemName = topicVoteItemName;
+        topicVotes = new ArrayList<>();
+    }
+
+    public void voteTopicVoteItem(TopicVote topicVote) {
+        topicVotes.add(topicVote);
+    }
+
+    public void cancelVote(TopicVote topicVote) {
+        for (int i = 0; i < topicVotes.size(); i++) {
+            TopicVote current = topicVotes.get(i);
+            if (current.getTopicVoteId().equals(topicVote.getTopicVoteId())) {
+                topicVotes.remove(current);
+            }
+        }
+    }
+
+    public Boolean isTopicVoteItemVoted(Long memberId) {
+        for (TopicVote topicVote : topicVotes) {
+            if (topicVote.getMember().getMemberId().equals(memberId)) {
+                topicVoteItemVoted = true;
+                return true;
+            }
+        }
+        topicVoteItemVoted = false;
+        return false;
     }
 }
