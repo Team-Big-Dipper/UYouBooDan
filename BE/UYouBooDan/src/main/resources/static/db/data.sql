c70897f43588b95509824d199c24a295430353aa
mingu0429 <73528227+mingu0429@users.noreply.github.com>
2023-02-09 23:38:59 +0900

    [BE] data.sql Member_roles 변경 (#97)
    Co-authored-by: mingu0429 <mingu0429@gmail.com>

--- BE/UYouBooDan/src/main/resources/static/db/data.sql
+++ BE/UYouBooDan/src/main/resources/static/db/data.sql
@@ -5,7 +5,7 @@ INSERT INTO Member(memberId, createdAt, last_modified_at, email, memberStatus, n
 (3, '2023-02-03 00:05:33.825730', '2023-02-03 00:05:33.825730', 'rlgywnd@naver.com', 'MEMBER_ACTIVE', 'Kakao 김효중', '{bcrypt}$2a$10$ps7EXP6V8pxDB0fgsBvbduRzcnHO5sD32FtFQ7tzBfcztBWSRTFIO', 'https://scontent-gmp1-1.xx.fbcdn.net/v/t1.18169-9/527016_499021583525593_732357164_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=CdbnqyyFWXkAX_obHCp&_nc_ht=scontent-gmp1-1.xx&oh=00_AfDBQSsLnCXMKoAPnGFrOeSBgvMp__vgjXLEqmtS6etfcw&oe=63F1DB6A'),
 (4, '2023-02-03 00:05:33.825730', '2023-02-03 00:05:33.825730', 'rlgywnd123@gmail.com', 'MEMBER_ACTIVE', 'Google 김효중', '{bcrypt}$2a$10$ps7EXP6V8pxDB0fgsBvbduRzcnHO5sD32FtFQ7tzBfcztBWSRTFIO', 'https://lh3.googleusercontent.com/a/AEdFTp60PjsqOwMeG52mdMrNG9kpqRrt7PXZBokb2sos=s96-c');
 
-INSERT INTO member_roles(Member_memberId,roles) values
+INSERT INTO Member_roles(Member_memberId,roles) values
 (1,'USER'),
 (2,'USER'),
 (3,'USER'),
@@ -24,7 +24,7 @@ INSERT INTO TopicVoteItem(topicVoteItemId, createdAt, last_modified_at, topicVot
 (5,'2023-02-01 00:05:33.825730','2023-02-01 00:05:33.825730', '투표 게시글 투표 항목1 테스트',3),
 (6,'2023-02-01 00:05:33.825730','2023-02-01 00:05:33.825730', '투표 게시글 투표 항목2 테스트',3);
 
-INSERT INTO TopicVote(topicVoteId, createdAt, last_modified_at, memberId, topicVoteItemId) VALUE
+INSERT INTO TopicVote(topicVoteId, createdAt, last_modified_at, member_id, topic_vote_item_id) VALUE
 (1,'2023-01-01 00:05:33.825730','2023-01-01 00:05:33.825730',1,1),
 (2,'2023-01-01 00:05:33.825730','2023-01-01 00:05:33.825730',2,1),
 (3,'2023-01-01 00:05:33.825730','2023-01-01 00:05:33.825730',3,2);
