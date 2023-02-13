ee20f22c35f69cbc0c1599760f3ba8f8fd23c2c1
Dongjin Kim <nyong9221@gmail.com>
2023-02-14 02:27:53 +0900

    :sparkles: google logout API 구현

--- BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/global/oauth2/google/GoogleOauthController.java
+++ BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/global/oauth2/google/GoogleOauthController.java
@@ -1,6 +1,9 @@
 package TeamBigDipper.UYouBooDan.global.oauth2.google;
 
+import TeamBigDipper.UYouBooDan.global.exception.dto.BusinessLogicException;
+import TeamBigDipper.UYouBooDan.global.exception.exceptionCode.ExceptionCode;
 import TeamBigDipper.UYouBooDan.global.security.jwt.JwtTokenizer;
+import TeamBigDipper.UYouBooDan.global.security.util.JwtExtractUtil;
 import TeamBigDipper.UYouBooDan.member.entity.Member;
 import TeamBigDipper.UYouBooDan.member.service.MemberService;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -21,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RequiredArgsConstructor
@@ -41,6 +45,7 @@ public class GoogleOauthController {
 
     private final MemberService memberService;
     private final JwtTokenizer jwtTokenizer;
+    private final JwtExtractUtil jwtExtractUtil;
 
     /**
      * 프론트 요청 API : 인증 code 받기용
@@ -141,6 +146,29 @@ public class GoogleOauthController {
         response.setHeader("RefreshToken", refreshToken);
 
         return "Success Login: User";
+    }
+
+
+    /**
+     * 구글 로그 아웃
+     * @param request
+     * @return
+     */
+    @GetMapping("/logout")
+    public ResponseEntity<?> googleLogout(HttpServletRequest request) {
+
+        Long memberId = jwtExtractUtil.extractMemberIdFromJwt(request);
+        String accessToken = jwtExtractUtil.extractAccessTokenFromJwt(request);
+        Long expiration = jwtExtractUtil.getExpiration(accessToken);
+
+        try {
+            // 구글 토큰 만료 시키기 위한 로직 추가 필요
+            memberService.verifyMemberFromRedis(memberId, accessToken, expiration);  // 자체 서비스 로그아웃 로직
+
+        } catch (Exception e) { throw new BusinessLogicException(ExceptionCode.NOT_FOUND); }
+
+
+        return new ResponseEntity<>("Success Logout: User", HttpStatus.OK);
     }
 
 
