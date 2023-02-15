f17ab44aa585b11cc3b3614f2098f1eb56aa0721
ZeroMin-K <ymkim9510@gmail.com>
2023-02-15 22:44:37 +0900

    :sparkles: feat: TopicPatchReqDto 클래스 생성자 추가 #121

--- BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/topic/dto/TopicPatchReqDto.java
+++ BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/topic/dto/TopicPatchReqDto.java
@@ -1,5 +1,8 @@
 package TeamBigDipper.UYouBooDan.topic.dto;
 
+import TeamBigDipper.UYouBooDan.member.entity.Member;
+import TeamBigDipper.UYouBooDan.topic.entity.Topic;
+import TeamBigDipper.UYouBooDan.topic.entity.TopicVoteItem;
 import lombok.Getter;
 
 import java.util.List;
@@ -17,6 +20,35 @@ public class TopicPatchReqDto {
 
     @Getter
     public static class TopicVoteItemPatchDto {
+        private Long topicVoteItemId;           // 투표 항목 ID
         private String topicVoteItemName;       // 투표 항목 이름
+    }
+
+    /**
+     * TopicPatchReqDto 클래스를 투표 게시글 Topic Entity 클래스로 변환하는 메서드
+     * @param member 작성자 Member 클래스
+     * @return Topic Entity 클래스
+     */
+    public Topic toEntity(Member member) {
+        Topic topic = Topic.builder()
+                .title(this.title)
+                .content(this.content)
+                .category(this.category)
+                .closedAt(this.closedAt)
+                .member(member)
+                .build();
+
+        // 투표 게시글에 투표 항목 추가
+        topicVoteItems.stream()
+                .map(topicVoteItemPatchDto -> {
+                    TopicVoteItem topicVoteItem = TopicVoteItem.builder()
+                            .topicVoteItemId(topicVoteItemPatchDto.getTopicVoteItemId())
+                            .topic(topic)
+                            .topicVoteItemName(topicVoteItemPatchDto.getTopicVoteItemName())
+                            .build();
+                    return topicVoteItem;})
+                .forEach(topicVoteItem -> topic.addTopicVoteItem(topicVoteItem));
+
+        return topic;
     }
 }
