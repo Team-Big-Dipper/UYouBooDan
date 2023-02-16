3ff2441b7ebf59af2d0cd6ea35921a9cd1ebbacc
ZeroMin-K <ymkim9510@gmail.com>
2023-02-17 00:03:20 +0900

    :sparkles: feat: TopicPatchDto 클래스내 투표 항목 수정 부분 주석 처리 #121
    - 투표 항목 수정 불가로 인해 투표 항목 수정 관련 로직 주석 처리

--- BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/topic/dto/TopicPatchReqDto.java
+++ BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/topic/dto/TopicPatchReqDto.java
@@ -9,20 +9,23 @@ import java.util.List;
 
 /**
  * 투표 게시글 수정에 대한 Request DTO 클래스
+ * 투표 항목은 일단 수정 불가 - 추후 유지 보수하면서 나중에 대비하여 투표 항목 부분 코드 주석 처리
  */
 @Getter
 public class TopicPatchReqDto {
     private String category;                            // 카테고리
     private String title;                               // 제목
     private String content;                             // 내용
-    private String closedAt;                            // 마감일
-    private List<TopicVoteItemPatchDto> topicVoteItems; // 투표 항목
+//    private List<TopicVoteItemPatchDto> topicVoteItems; // 투표 항목
 
-    @Getter
-    public static class TopicVoteItemPatchDto {
-        private Long topicVoteItemId;           // 투표 항목 ID
-        private String topicVoteItemName;       // 투표 항목 이름
-    }
+    /**
+     * 투표 항목 Patch DTO 클래스
+     */
+//    @Getter
+//    public static class TopicVoteItemPatchDto {
+//        private Long topicVoteItemId;           // 투표 항목 ID
+//        private String topicVoteItemName;       // 투표 항목 이름
+//    }
 
     /**
      * TopicPatchReqDto 클래스를 투표 게시글 Topic Entity 클래스로 변환하는 메서드
@@ -34,20 +37,19 @@ public class TopicPatchReqDto {
                 .title(this.title)
                 .content(this.content)
                 .category(this.category)
-                .closedAt(this.closedAt)
                 .member(member)
                 .build();
 
         // 투표 게시글에 투표 항목 추가
-        topicVoteItems.stream()
-                .map(topicVoteItemPatchDto -> {
-                    TopicVoteItem topicVoteItem = TopicVoteItem.builder()
-                            .topicVoteItemId(topicVoteItemPatchDto.getTopicVoteItemId())
-                            .topic(topic)
-                            .topicVoteItemName(topicVoteItemPatchDto.getTopicVoteItemName())
-                            .build();
-                    return topicVoteItem;})
-                .forEach(topicVoteItem -> topic.addTopicVoteItem(topicVoteItem));
+//        topicVoteItems.stream()
+//                .map(topicVoteItemPatchDto -> {
+//                    TopicVoteItem topicVoteItem = TopicVoteItem.builder()
+//                            .topicVoteItemId(topicVoteItemPatchDto.getTopicVoteItemId())
+//                            .topic(topic)
+//                            .topicVoteItemName(topicVoteItemPatchDto.getTopicVoteItemName())
+//                            .build();
+//                    return topicVoteItem;})
+//                .forEach(topicVoteItem -> topic.addTopicVoteItem(topicVoteItem));
 
         return topic;
     }
