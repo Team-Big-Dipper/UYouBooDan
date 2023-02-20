24eb85434cc113f63c549e21acb7cd98b9ddbb55
ZeroMin-K <ymkim9510@gmail.com>
2023-02-20 21:32:27 +0900

    :bug: fix: TopicPostReqDto 클래스에 유효성 검증 추가 #145
    - category, title, closedAt 필드에 @NotBlank 추가
    - closedAt 필드에 날짜형식 유효성 검증위한 커스텀 어노테이션 @ValidDate 추가
    - 투표항목 리스트 topicVoteItems 필드에 @Valid, @NotNull, @Size(min = 1) 추가
    - 투표항목이름 topicVoteItemName에 @NotBlank 추가

--- BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/topic/dto/TopicPostReqDto.java
+++ BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/topic/dto/TopicPostReqDto.java
@@ -3,9 +3,14 @@ package TeamBigDipper.UYouBooDan.topic.dto;
 import TeamBigDipper.UYouBooDan.member.entity.Member;
 import TeamBigDipper.UYouBooDan.topic.entity.Topic;
 import TeamBigDipper.UYouBooDan.topic.entity.TopicVoteItem;
+import TeamBigDipper.UYouBooDan.topic.util.validator.ValidDate;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -13,16 +18,26 @@ import java.util.stream.Collectors;
 
 @Getter
 public class TopicPostReqDto {
+    @NotBlank(message = "카테고리는 공백이 아니어야 합니다.")
     private String category;                // 카테고리
+
+    @NotBlank(message = "제목은 공백이 아니어야 합니다.")
     private String title;                   // 제목
     private String content;                 // 내용
+
+    @NotBlank(message = "마감일은 공백이 아니어야 합니다.")
+    @ValidDate(message = "마감일의 날짜 형식은 년:월:일 시:분:초 입니다")
     private String closedAt;         // 마감일
 
+    @Valid
+    @NotNull
+    @Size(min = 1)
     private List<TopicVoteItemPostDto> topicVoteItems;    // 투표 항목
 
     // 투표 항목에 대한 DTO 클래스
     @Getter
     public static class TopicVoteItemPostDto {
+        @NotBlank(message = "투표 항목은 공백이 아니어야 합니다.")
         private String topicVoteItemName;       // 투표 항목 이름
     }
 
