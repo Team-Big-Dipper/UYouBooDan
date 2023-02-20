e3f3bbd9f6d3861bdc4575c6b44dc34d95682fbe
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2023-02-20 22:03:40 +0900

    [BE] 투표 게시글 작성, 조회, 투표시 요청에 대한 유효성 검증 추가 # 145 (#146)

--- BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/topic/dto/TopicVoteReqDto.java
+++ BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/topic/dto/TopicVoteReqDto.java
@@ -2,11 +2,16 @@ package TeamBigDipper.UYouBooDan.topic.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
 /**
  * 투표시 Request DTO 클래스
  */
 @Getter
 public class TopicVoteReqDto {
 
+    @NotBlank(message = "투표하는 투표 항목은 공백이 아니어야 합니다")
+    @Min(1)
     private Long topicVoteItemId;       // 투표 항목 ID
 }
