03bf620d638710de8337e5f7f4183c455b950c12
NR0617 <nara20210617@gmail.com>
2023-03-09 00:12:15 +0900

    :sparkles: feat: 게시글 수정, 삭제 기능 제작

--- FE/src/components/ReadVote/voteBtn.tsx
+++ FE/src/components/ReadVote/voteBtn.tsx
@@ -2,36 +2,46 @@ import React, { useState } from 'react';
 import * as S from './style';
 import ButtonModal from '../commons/buttonModal';
 import { useRouter } from 'next/router';
+import { deletevote } from '../../apis/readvote/readvote';
+import { getToken } from '../../utils/userToken';
 
-interface propsType {
+interface propTypes {
   isAuthor: boolean | undefined;
+  topicId: string | string[] | undefined;
+  isClosed: boolean | undefined;
 }
 
-const VoteBtn = ({ isAuthor }: propsType) => {
-  const [login, setIsLogin] = useState(true);
+const VoteBtn = ({ isAuthor, topicId, isClosed }: propTypes) => {
+  //console.log(isAuthor, topicId, isClosed);
   const [askText, setAskText] = useState('');
-  const [apiMethod, setApiMethod] = useState('');
   const router = useRouter();
-
+  const usertoken = getToken();
   const [openModal, setOpenModal] = useState(false);
-  const onClickBtn = (e: any) => {
-    setOpenModal((prev) => !prev);
-    if (e.target.id === 'delete') {
-      setApiMethod('delete');
-      setAskText('삭제할까요?');
-    } else if (e.target.id === 'patch') {
-      setApiMethod('patch');
-      setAskText('수정할까요?');
-    }
-  };
+
   const handleLink = () => {
     router.push('/voteList');
   };
   const handleDelete = () => {
-    console.log('delete');
+    if (topicId !== undefined && usertoken !== undefined && isAuthor) {
+      deletevote(Number(topicId), usertoken);
+      router.back();
+    }
   };
   const handlePatch = () => {
-    console.log('patch');
+    if (topicId !== undefined && usertoken !== undefined && isAuthor) {
+      router.push({
+        pathname: '/updatevote',
+        query: { pid: topicId },
+      });
+    }
+  };
+  const onClickBtn = (e: any) => {
+    if (e.target.id === 'delete') {
+      setOpenModal((prev) => !prev);
+      setAskText('삭제할까요?');
+    } else if (e.target.id === 'patch') {
+      handlePatch();
+    }
   };
   return (
     <S.ReadVoteBtnContainer>
@@ -48,6 +58,13 @@ const VoteBtn = ({ isAuthor }: propsType) => {
               <S.ReadVoteBtn id="delete" onClick={onClickBtn}>
                 삭제하기
               </S.ReadVoteBtn>
+              <>
+                {isClosed ? null : (
+                  <S.ReadVoteBtn id="patch" onClick={onClickBtn} color={'gray'}>
+                    수정하기
+                  </S.ReadVoteBtn>
+                )}
+              </>
               <S.ReadVoteBtn
                 id="votelist"
                 onClick={handleLink}
@@ -60,9 +77,7 @@ const VoteBtn = ({ isAuthor }: propsType) => {
               {openModal ? (
                 <ButtonModal
                   text={askText}
-                  confirmFunc={
-                    apiMethod === 'patch' ? handlePatch : handleDelete
-                  }
+                  confirmFunc={handleDelete}
                   setOpenModal={setOpenModal}
                 />
               ) : null}
