55ca355e8ec3e93e79b6b1e497846d3fd78e0f14
NR0617 <nara20210617@gmail.com>
2023-04-02 22:22:06 +0900

    :sparkles: feat: 페이지버튼 수정, console.log제거

--- FE/src/apis/voteInstance.ts
+++ FE/src/apis/voteInstance.ts
@@ -1,17 +1,8 @@
-import axios, { AxiosError } from 'axios';
+import axios from 'axios';
 
 const voteInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
   timeout: 5000,
 });
-
-// voteInstance.interceptors.response.use(async (error) => {
-//   if (error instanceof AxiosError) {
-//     if (error.response?.status === 401) {
-//       return '잘못된 요청입니다';
-//     }
-//   }
-//   return Promise.reject(error.response);
-// });
 
 export default voteInstance;
