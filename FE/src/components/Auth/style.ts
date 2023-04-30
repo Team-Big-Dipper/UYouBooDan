216b311c49986c4b9ecd8870688af91a7a08f739
kimhyojung <rlgywnd123@gmail.com>
2023-05-01 05:41:55 +0900

    :lipstick: Login 반응형 완료

--- FE/src/components/Auth/style.ts
+++ FE/src/components/Auth/style.ts
@@ -2,50 +2,74 @@ import styled from 'styled-components';
 import Link from 'next/link';
 
 export const AuthContainer = styled.div`
-  padding: 10% 7%;
   border: 1px solid #d6d9dc;
   border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 450px;
+  height: 100%;
+  padding: 50px 0 50px 0;
+  @media (max-width: 500px) {
+    width: 350px;
+    padding: 20px 0 20px 0;
+  }
 `;
 export const AuthLogo = styled.div`
   font-weight: 800;
-  font-size: 2.4rem;
   color: #4285f4;
   display: inline-block;
-  padding: 0.5vh 0.5vh 0.2vh 0.5vh;
   background-color: #f3f3f3;
-  margin-bottom: 3vh;
+  font-size: 38px;
+  padding: 3px;
+  margin-bottom: 20px;
+  @media (max-width: 500px) {
+    font-size: 27px;
+    margin-bottom: 15px;
+  }
 `;
 export const LoginTitle = styled.div`
   font-weight: 400;
-  font-size: 1.3rem;
-  margin-bottom: 4vh;
+  font-size: 21px;
+  margin-bottom: 33px;
+  @media (max-width: 500px) {
+    font-size: 19px;
+    margin-bottom: 27px;
+  }
   span {
     display: inline-block;
-    margin-top: 1vh;
+    margin-top: 10px;
     font-weight: 800;
     color: #4285f4;
+    @media (max-width: 500px) {
+      font-size: 19px;
+    }
   }
 `;
 // email
 export const LoginEmailContainer = styled.div`
-  margin-bottom: 3.5vh;
+  margin-bottom: 28px;
 `;
 export const LoginEmailTitle = styled.div`
   font-weight: bold;
   color: #667085;
-  font-size: 1rem;
-  margin-bottom: 1vh;
+  font-size: 16px;
+  margin-bottom: 9px;
 `;
 export const LoginEmailInputDiv = styled.div`
   border: 1px solid #d6d9dc;
   display: flex;
   justify-content: space-between;
-  width: 25.67vw;
-  height: 6.5vh;
+  width: 375px;
+  height: 53px;
   border-radius: 9px;
-  padding: 0 2%;
+  padding: 0 8px 0 8px;
+  @media (max-width: 500px) {
+    width: 320px;
+  }
   input {
-    font-size: 1rem;
+    font-size: 16px;
     width: 80%;
     border: none;
     outline: none;
@@ -59,25 +83,28 @@ export const LoginEmailDeleteDiv = styled.div`
 
 // password
 export const PwContainer = styled.div`
-  margin-bottom: 2.5vh;
+  margin-bottom: 20px;
 `;
 export const PwTitle = styled.div`
   font-weight: bold;
   color: #667085;
-  font-size: 1rem;
-  margin-bottom: 1vh;
+  font-size: 16px;
+  margin-bottom: 9px;
 `;
 export const PwInputDiv = styled.div`
   border: 1px solid #d6d9dc;
   display: flex;
   justify-content: space-between;
-  width: 25.67vw;
-  height: 6.5vh;
+  width: 375px;
+  height: 53px;
   border-radius: 9px;
-  padding: 0 2%;
-  margin-bottom: 1vh;
+  padding: 0 8px 0 8px;
+  margin-bottom: 9px;
+  @media (max-width: 500px) {
+    width: 320px;
+  }
   input {
-    font-size: 1rem;
+    font-size: 16px;
     color: black;
     width: 80%;
     border: none;
@@ -97,7 +124,7 @@ export const PwVectorDiv = styled.div`
 
 // 유효성 msg
 export const LoginValidMsg = styled.div`
-  font-size: 0.86rem;
+  font-size: 13px;
   color: #ff2f2f;
   display: flex;
   align-items: center;
@@ -107,41 +134,44 @@ export const LoginValidMsg = styled.div`
 export const ContinueCheckBoxDiv = styled.div`
   display: flex;
   align-items: center;
-  margin-bottom: 1.5vh;
-  font-size: 1rem;
   color: #667085;
-  gap: 0.5vw;
+  margin-bottom: 12px;
+  font-size: 16px;
+  gap: 7px;
 `;
 
 // 아이디찾기, 비밀번호찾기, 회원가입 버튼
 export const SearchAndSignUpDiv = styled.div`
   display: flex;
   justify-content: space-between;
-  font-size: 0.85rem;
+  font-size: 14px;
   color: #667085;
-  margin-bottom: 3.5vh;
+  margin-bottom: 28px;
 `;
 export const SearchDiv = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  height: 3vh;
-  gap: 0.7vw;
+  height: 23px;
+  gap: 10px;
+  @media (max-width: 500px) {
+    font-size: 13px;
+  }
 `;
 export const SearchId = styled.div`
   display: flex;
   align-items: center;
-  height: 2vh;
+  height: 15px;
   cursor: pointer;
 `;
 export const SearchDecoDiv = styled.div`
   border-right: 1px solid #667085;
-  height: 2vh;
+  height: 15px;
 `;
 export const SearchPw = styled.div`
   display: flex;
   align-items: center;
-  height: 2vh;
+  height: 15px;
   cursor: pointer;
 `;
 export const SignUpBtnDiv = styled(Link)`
@@ -149,17 +179,20 @@ export const SignUpBtnDiv = styled(Link)`
   align-items: center;
   text-decoration: underline;
   color: #667085;
+  @media (max-width: 500px) {
+    font-size: 13px;
+  }
 `;
 // 로그인 버튼
 export const LoginBtnDiv = styled.div`
-  margin-bottom: 3vh;
+  margin-bottom: 53px;
   button {
     width: 100%;
-    height: 7vh;
+    height: 57px;
     border: none;
     border-radius: 9px;
     background-color: #4285f4;
-    font-size: 1.2rem;
+    font-size: 19px;
     color: white;
     cursor: pointer;
     transition-duration: 0.3s;
@@ -173,13 +206,17 @@ export const LoginBtnDiv = styled.div`
 export const SnsLoginTitleDiv = styled.div`
   display: flex;
   justify-content: space-between;
-  height: 3.5vh;
-  margin-bottom: 4vh;
+  gap: 10px;
+  height: 28px;
+  margin-bottom: 32px;
 `;
 export const FirstDeco = styled.div`
   border-bottom: 1px solid #d6d9dc;
-  width: 9.1vw;
-  height: 2.1vh;
+  width: 135px;
+  height: 15px;
+  @media (max-width: 500px) {
+    width: 100px;
+  }
 `;
 export const SnsLoginTitle = styled.div`
   display: flex;
@@ -189,15 +226,18 @@ export const SnsLoginTitle = styled.div`
 `;
 export const SecondDeco = styled.div`
   border-bottom: 1px solid #d6d9dc;
-  width: 9.1vw;
-  height: 2.1vh;
+  width: 135px;
+  height: 15px;
+  @media (max-width: 500px) {
+    width: 100px;
+  }
 `;
 
 // 소셜 로그인 Div
 export const SnsLoginContainer = styled.div`
   display: flex;
   justify-content: center;
-  gap: 2vw;
+  gap: 20px;
 `;
 export const KaKaoLoginDiv = styled.div`
   cursor: pointer;
--- FE/src/pages/auth/style.ts
+++ FE/src/pages/auth/style.ts
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 
 export const AuthPageContainer = styled.div`
   flex: 1;
-  border: 1px solid red;
   display: flex;
   justify-content: center;
   align-items: center;
   padding: 50px 0 50px 0;
+  // git push origin Feat#39 까지 해둔 상태 04/30 새벽 5시 33분
 `;
