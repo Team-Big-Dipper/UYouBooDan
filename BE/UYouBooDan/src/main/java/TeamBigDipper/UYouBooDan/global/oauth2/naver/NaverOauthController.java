48a6a91df2467ceed8c96693b2d67459c50155c7
Dongjin Kim <nyong9221@gmail.com>
2023-05-07 14:42:54 +0900

    :sparkles: feat: Naver Login API Callback API 중 토큰 발급 완성

--- BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/global/oauth2/naver/NaverOauthController.java
+++ BE/UYouBooDan/src/main/java/TeamBigDipper/UYouBooDan/global/oauth2/naver/NaverOauthController.java
@@ -4,15 +4,18 @@ import TeamBigDipper.UYouBooDan.global.security.jwt.JwtTokenizer;
 import TeamBigDipper.UYouBooDan.global.security.util.JwtExtractUtil;
 import TeamBigDipper.UYouBooDan.member.service.MemberService;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -29,6 +32,10 @@ public class NaverOauthController {
     @Getter
     @Value("${oauth.naver.clientId}")
     private String naverClientId;
+
+    @Getter
+    @Value("${oauth.naver.clientSecret}")
+    private String naverClientSecret;
 
     private final MemberService memberService;
     private final JwtTokenizer jwtTokenizer;
@@ -64,8 +71,36 @@ public class NaverOauthController {
      */
     @GetMapping("/callback")
     public String naverLogin(@RequestParam("code") String code, @RequestParam("state") String state, HttpServletResponse response) throws JsonProcessingException {
-        System.out.println("Check");
-        // 네이버 로그인 콜백 로직
+        // 네이버 로그인 Token 발급 API 요청을 위한 header/parameters 설정 부분
+        RestTemplate restTemplate = new RestTemplate(); // REST API 요청용 Template
+
+        HttpHeaders naverTokenRequestHeadres = new HttpHeaders();  // Http 요청을 위한 헤더 생성
+        naverTokenRequestHeadres.add("Content-type", "application/x-www-form-urlencoded"); // application/json 했다가 grant_type missing 오류남 (출력포맷만 json이라는 거엿음)
+
+        // 파라미터들을 담아주기위한 맵 (파라미터용이기 때문에, 따로 앞에 ?나 &나 =같은 부호를 입력해주지 않아도 됨. 오히려 넣으면 인식못함)
+        // 네이버 가이드에서 요청하는 파라미터들 (Developers 참고)
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", getNaverClientId());
+        params.add("client_secret", getNaverClientSecret());
+        params.add("code", code);
+        params.add("state", state);
+
+        HttpEntity<MultiValueMap<String, String>> naverTokenRequest =
+                new HttpEntity<>(params, naverTokenRequestHeadres);
+
+        // 서비스 서버에서 네이버 인증 서버로 요청 전송(POST 또는 GET이라고 공식문서에 있음), 응답은 Json으로 제공됨
+        ResponseEntity<String> oauthTokenResponse = restTemplate.exchange(
+                "https://nid.naver.com/oauth2.0/token",
+                HttpMethod.POST,
+                naverTokenRequest,
+                String.class
+        );
+
+        // 이제 우리가 사용할 적용하는 로직 작성
+        ObjectMapper objectMapper = new ObjectMapper();
+
+
 
         return "Success Logout: User";
     }
